98dc69815915a06700e0e614fa3e8b00ccdf1eeb
JB <josh@domumdigital.com>
2025-03-16 14:10:26 -0400

    script updates

---
+++ foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// Naming the specific contracts from the folders to be accurate during our import.
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract deploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
