3e5d74f4754c1c815fd3dcb0465bb2a253b662f3
JB <josh@domumdigital.com>
2025-03-20 08:19:41 -0400

    helper config script

--- foundry-fund-me/script/HelperConfig.s.sol
+++ foundry-fund-me/script/HelperConfig.s.sol
@@ -4,3 +4,24 @@ pragma solidity ^0.8.18;
 
 // 1. Deploy Mocks on our local Anvil chain
 // 2. Keep track of contract addresses across chains
+
+import {Script, console} from "../lib/forge-std/src/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+// If we are on our local Anvil chain, deploy Mock Price Feed addresses
+// If we are on a live network chain, deploy the real Price Feed addresses
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address PriceFeed;
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // Sepolia ETH Price Feed Address
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // Anvil ETH Price Feed Address
+    }
+}
